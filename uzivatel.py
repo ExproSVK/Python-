ce60b3ce35560304d518048b0a5050121ef9b2db
Adrian Toman <adkotoman@gmail.com>
2022-09-22 15:28:59 +0200

    USERS

---
+++ uzivatel.py
@@ -0,0 +1,13 @@
+from xml.etree.ElementInclude import DEFAULT_MAX_INCLUSION_DEPTH
+
+
+class Uzivatel:
+    def __init__(self, first_name, last_name, age) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        pass
+
+    def print_info(self) -> str:
+        return print(f"Meno: {self.first_name} Priezvisko: { self.last_name}")
+        
