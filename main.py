0f40924eba47c5e137b03bf38ebafb0bfd3ad37d
Adrian Toman <adkotoman@gmail.com>
2022-09-22 15:33:36 +0200

    a

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from uzivatel import Uzivatel
 
 
 print("Hello")
-
-user1 = Uzivatel("Adrian", "Toman", "20")
-
+user1 = Uzivatel("Adrian", "Toman", 20)
+user2 = Uzivatel("Fero", "Svarny",12)
+user1.print_info()
+user1= user2
 user1.print_info()
