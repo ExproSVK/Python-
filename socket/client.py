dad68cdedcdafeee65c6453b59d1df25a98e83a8
Adrian Toman <adkotoman@gmail.com>
2022-10-06 14:59:16 +0200

    Client oprava

--- socket/client.py
+++ socket/client.py
@@ -1,15 +1,10 @@
-import client
-import imp
 import socket
 
+import ClientConfig
 
-from ClientConfig import ClientConfig
+client_config = ClientConfig.ClientConfig()
 
-client_config = ClientConfig()
-
-
-
-client_socket = socket.socket((socket.AF_INET, socket.SOCK_STREAM))
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((client_config.server_ip_address, client_config.server_port))
 
 client_socket.send("Hello!!!".encode())
