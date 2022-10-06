274adfc771097c3a9fc74b88ae9c5705f9991491
Adrian Toman <adkotoman@gmail.com>
2022-10-06 15:46:19 +0200

    ALL

--- socket/ActionEnum.py
+++ socket/ActionEnum.py
@@ -4,7 +4,7 @@ from enum import IntEnum
 
 from enum import IntEnum
 
-class Actions(IntEnum):
+class ActionEnum(IntEnum):
     LOGIN = 1
     EXIT = 2
     USERS = 3
--- socket/server.py
+++ socket/server.py
@@ -1,14 +1,34 @@
+from argparse import Action
 from concurrent.futures import thread
 from http import client, server
+import json
 import socket
 import threading
 from ServerConfig import ServerConfig
+from MessageModel import MessageModel
+from ActionEnum import ActionEnum
 
+users = list()
 def handle_client(pa_client_socket, pa_client_address):
-    #while(True):
+    while(True):
         data = pa_client_socket.recv(1500)
-        print(data)
-    #pass
+        msg:MessageModel = json.loads(data.decode(), object_hook=MessageModel.from_json)
+
+        match msg.action:
+            case ActionEnum.LOGIN:
+                users.append(msg.from_user)
+                print(f"USer: {msg.from_user} are log in. IP: {pa_client_address[0]}:{pa_client_address[1]}")
+                continue
+            case ActionEnum.EXIT:
+                users.remove(msg.from_user)
+                pa_client_socket.close()
+                return
+            case ActionEnum.USERS:
+                msg_to_user = MessageModel("Server", "", users,ActionEnum.USERS)
+                json_str = json.dumps(msg_to_user, __dict__)
+                pa_client_socket.send(json_str)
+                continue
+        print(f"Message {pa_client_address[0]}:{pa_client_address[1]} from ")
 
 server_config = ServerConfig()
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
