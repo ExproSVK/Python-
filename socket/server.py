e8115cc664d544615545d5d4b8315da8350dac18
Adrian Toman <adkotoman@gmail.com>
2022-10-06 14:53:25 +0200

    a

--- socket/server.py
+++ socket/server.py
@@ -5,17 +5,17 @@ import threading
 from ServerConfig import ServerConfig
 
 def handle_client(pa_client_socket, pa_client_address):
-    while(True):
+    #while(True):
         data = pa_client_socket.recv(1500)
         print(data)
-    pass
+    #pass
 
 server_config = ServerConfig()
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server_socket.bind((server_config.ip_addres, server_config.port))
-server_socket.listen("10")
+server_socket.bind((server_config.ip_address, server_config.port))
+server_socket.listen(10)
 
-print(f"Server is running on IP {server_config.ip_addres}, {server_config.port}")
+print(f"Server is running on IP {server_config.ip_address}, {server_config.port}")
 
 while(True):
     (client_socket, client_address) = server_socket.accept()
